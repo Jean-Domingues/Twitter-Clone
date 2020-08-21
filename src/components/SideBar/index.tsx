66e30fb5b442853bfb67b1c0d1123aaf8df583f8
Jean Domingues <jeandomingues752@gmail.com>
2020-08-21 17:20:28 -0300

    Criado o componente de lista

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Container, SearchWrapper, SearchInput, SearchIcon, Body } from './styles';
+import List from '../List';
 
 const SideBar: React.FC = () => {
   return (
@@ -11,18 +12,8 @@ const SideBar: React.FC = () => {
       </SearchWrapper>
 
       <Body>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel doloribus facilis
-          corporis nisi necessitatibus asperiores itaque, earum illum cumque laboriosam
-          porro at id officiis dolorem rem ullam inventore! Iusto odit officiis tempora
-          inventore qui nostrum consequuntur quam dolores fugiat, corporis possimus
-          repudiandae praesentium facilis adipisci similique molestiae totam tenetur!
-          Deleniti aut quae officia id neque accusantium provident accusamus repellat
-          animi debitis fuga dolorum, quaerat assumenda ipsum cumque eligendi iure
-          dignissimos odio omnis dicta nisi! Quod excepturi quos est, natus rerum
-          cupiditate. Repellat quisquam, rerum molestias dolore voluptatum iusto
-          recusandae nesciunt!
-        </p>
+        <List title="Talvez você curta" elements={[<h1>Teste</h1>, <h1>Teste</h1>]} />
+        <List title="Talvez você curta" elements={[<h1>Teste</h1>, <h1>Teste</h1>]} />
       </Body>
     </Container>
   );
